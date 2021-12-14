a18e746e0531300f49b72836144692558a5745c4
Vyacheslav <vyacheslavlutsenkoml@gmail.com>
2021-12-14 22:13:37 +0500

    Создание свайпера
    Подключил библиотеку swiper.js. Создал базовый свайпер

--- #src/assets/scripts/script.js
+++ #src/assets/scripts/script.js
@@ -1,2 +1,4 @@
-import * as functional from './functional.js'
+import * as functional from './functional.min.js'
+import * as swiper from './swiper.min.js'
 functional.dropdownFn()
+swiper.swiperFn()
---
+++ dist/assets/scripts/swiper.js
@@ -0,0 +1,34 @@
+import Swiper from 'https://unpkg.com/swiper@7/swiper-bundle.esm.browser.min.js'
+
+export const swiperFn = () => {
+	const swiper = new Swiper('.-hm-simple-swiper', {
+		loop: true,
+	
+		// Navigation arrows
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+		
+		breakpoints: {
+			// when window width is >= 320px
+			319.89: {
+				slidesPerView: 1,
+				spaceBetween: 60
+			},
+			1023.89: {
+				slidesPerView: 2,
+				spaceBetween: 60
+			},
+			1439.89: {
+				slidesPerView: 3,
+				spaceBetween: 60
+			},
+			1919.89: {
+				slidesPerView: 4,
+				spaceBetween: 120
+			}
+		}
+	});
+	
+} 
