8373a10b6fe4df3b026196f2c19ff56648a99418
Vyacheslav <vyacheslavlutsenkoml@gmail.com>
2021-12-16 18:38:19 +0500

    bugfix#

--- dist/assets/scripts/sliders.js
+++ dist/assets/scripts/sliders.js
@@ -1,24 +1,26 @@
 // import noUiSlider from '../helpers/nouislider.min.js';
 export const sliderFn = () => {
 	var stepsSlider = document.getElementById('slider-price');
-	var input0 = document.getElementById('byPrice');
-	var input1 = document.getElementById('toPrice');
-	var inputs = [input0, input1];
-
-	noUiSlider.create(stepsSlider, {
-			start: [0, 1000],
-			connect: true,
-			
-			format: wNumb({
-        decimals: 0,
-			}),
-			range: {
-					'min': [0],
-					'max': 100000
-			}
-	});
-
-	stepsSlider.noUiSlider.on('update', function (values, handle) {
-			inputs[handle].value = values[handle];
-	});
+	if(stepsSlider !== null) {
+		var input0 = document.getElementById('byPrice');
+		var input1 = document.getElementById('toPrice');
+		var inputs = [input0, input1];
+	
+		noUiSlider.create(stepsSlider, {
+				start: [0, 1000],
+				connect: true,
+				
+				format: wNumb({
+					decimals: 0,
+				}),
+				range: {
+						'min': [0],
+						'max': 100000
+				}
+		});
+	
+		stepsSlider.noUiSlider.on('update', function (values, handle) {
+				inputs[handle].value = values[handle];
+		});
+	}
 }
